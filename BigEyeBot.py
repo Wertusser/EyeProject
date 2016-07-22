e1f51502a4ce9d6b9e22167da18e6bcb1d4f1056
Wertusser <wertusser@gmail.com>
2016-07-22 17:11:48 +0500

    first commit

---
+++ BigEyeBot.py
@@ -0,0 +1,13 @@
+import datetime,time
+import threading
+from adapters import infoVk
+
+######  Main loop ######
+
+def main():
+    a=datetime.datetime.now()
+    print(datetime.datetime.now()-a)
+
+
+if __name__ == '__main__':
+    main()
